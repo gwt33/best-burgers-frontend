07fbb209e510771fd5968c8d09ddad2fa3243a9c
93geraldtee <93geraldtee@gmail.com>
2020-07-11 01:02:29 -0400

    remove extra files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App() extends React.Component {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  
+  // async get request
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/restaurants')
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        App
+      </div>
+    );
+  }
 }
 
 export default App;
