1e0b1d1b4bbf7687de525a3991c1cfc50a1a8a3a
93geraldtee <93geraldtee@gmail.com>
2020-07-12 17:43:03 -0400

    add form for a new restaurant, handleChange and handleSubmit methods

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -1,11 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import {createRestaurant} from '../actions/createRestaurant'
 
-export default class RestaurantInput extends Component {
+class RestaurantInput extends React.Component {
+
+    state = {
+        name: '',
+        location: ''
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value,
+            [event.target.location]: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.props.createRestaurant(this.state)
+    }
+
+    // setup form for creating new restaurant
     render() {
         return (
             <div>
-                RestaurantInput
+                <form onSubmit={this.handleSubmit}>
+                    <h3>Create new Restaurant!</h3> 
+                    <input type='text' placeholder='Name' value={this.state.name} name="name" onChange={this.handleChange}/>
+                    <input type='text' placeholder='Location' value={this.state.location} name="location" onChange={this.handleChange}/> <br/>
+                    <input type='submit'/>
+                </form>
             </div>
         )
     }
 }
+
+export default connect(null, {createRestaurant})(RestaurantInput)
