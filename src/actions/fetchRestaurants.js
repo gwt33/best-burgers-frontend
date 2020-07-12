10ff3f2c839bb4a456833fc61a61f860eb52ee1d
93geraldtee <93geraldtee@gmail.com>
2020-07-12 15:14:06 -0400

    add a get fetch request to api for restaurants

--- src/actions/fetchRestaurants.js
+++ src/actions/fetchRestaurants.js
@@ -1,7 +1,11 @@
-export function fetchRestaurants() {
-
-    fetch('http://localhost:3000/api/v1/restaurants')
-        .then(response => response.json())
-        .then(data => console.log(data))
-
+export default function fetchRestaurants() {
+    return (dispatch) => {
+        fetch('http://localhost:3000/api/v1/restaurants')
+            .then(response => response.json())
+            .then(restaurantsJSON => dispatch({
+                type: 'FETCH_RESTAURANTS',
+                payload: restaurantsJSON
+                })
+            )
+    }
 }
