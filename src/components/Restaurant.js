3fdc486b2fb8784b7310b96a1f1d6a28c1ca96a0
93geraldtee <93geraldtee@gmail.com>
2020-07-12 20:19:24 -0400

    add Restaurant component

---
+++ src/components/Restaurant.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import Restaurants from './Restaurants';
+
+const Restaurant = (props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Restaurant;
