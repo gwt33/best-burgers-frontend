eb9e2963a0719787ff444c21fd0fc47d77d1db92
93geraldtee <93geraldtee@gmail.com>
2020-07-13 06:43:39 -0400

    add code to find restaurant id

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import {Redirect} from 'react-router-dom'
+import BurgersContainer from '../containers/BurgersContainer'
 
 const Restaurant = (props) => {
 
     let restaurant = props.restaurants[props.match.params.id - 1]
+    // let restaurant = props.restaurants.filter(restaurant => restaurant.id == props.match.params.id)
 
     return (
-        <li>
-            {restaurant ? restaurant.name : null} - {restaurant ? restaurant.location : null}
-        </li>
+        <div>
+            {restaurant ? null : <Redirect to='/restaurants'/>}
+            <h2>{restaurant ? restaurant.name : null} - {restaurant ? restaurant.location : null}</h2>
+            <BurgersContainer restaurant={restaurant}/>
+        </div>
     )
 }
 
