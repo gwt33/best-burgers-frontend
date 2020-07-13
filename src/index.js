8c103e46ddc7f907551bb0698e596197c62d5f00
93geraldtee <93geraldtee@gmail.com>
2020-07-12 22:06:51 -0400

    wrap App component with Router and imported Router

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import {Provider} from 'react-redux';
 import restaurantReducer from './reducers/restaurantReducer';
+import {BrowserRouter as Router} from 'react-router-dom'
 //
 
 import App from './App';
@@ -19,12 +20,10 @@ let store = createStore(restaurantReducer, composeEnhancers(applyMiddleware(thun
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
