50a62ea5529cc1f66818eda77ae8ba3667a7ddc6
93geraldtee <93geraldtee@gmail.com>
2020-07-12 20:19:58 -0400

    add create restaurant action

--- src/actions/createRestaurant.js
+++ src/actions/createRestaurant.js
@@ -1,9 +1,22 @@
+// POST action to create a new restaurant
+
 export const createRestaurant = (data) => {
 
     return (dispatch) => {
-
-        
-
+        fetch('http://localhost:3000/api/v1/restaurants', {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify(data)
+        })
+            .then(response => response.json())
+            .then(restaurantJson => dispatch({
+                //this dispatches to the reducer
+                type: 'CREATE_RESTAURANT',
+                payload: restaurantJson
+            })
+        )
     }
-
 }
