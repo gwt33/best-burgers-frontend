19261e68de7fdc19ed330fd0c21319e2fac62b28
93geraldtee <93geraldtee@gmail.com>
2020-07-13 06:44:48 -0400

    update syntax for Link

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import {Route, Link} from 'react-router-dom';
 import Restaurant from './Restaurant';
 
 // renders each restaurant
 const Restaurants = (props) => {
     return (
         <div>
-            {props.restaurants.map(restaurant => <div key={restaurant.id}><Restaurant restaurant={restaurant}/></div> )}
+        <h1>Restaurants</h1>
+            {props.restaurants.map(restaurant => 
+                <li key={restaurant.id}>
+                    <Link to={`/restaurants/${restaurant.id}`}>{restaurant.name} - {restaurant.location}</Link>
+                </li> )}
         </div>
     )
 
