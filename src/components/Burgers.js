3c22d4634ee97cdc9d36bbd8b35c7138ce6c3f2c
93geraldtee <93geraldtee@gmail.com>
2020-07-13 20:21:52 -0400

    removed delete button and function to debug later

--- src/components/Burgers.js
+++ src/components/Burgers.js
@@ -1,18 +1,27 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {deleteBurger} from '../actions/deleteBurger'
+
 
 const Burgers = (props) => {
 
-    console.log(props)
+    console.log(props.burgers)
+
+    // const handleDelete = (burger) => {
+    //     this.props.deleteBurger(burger.id, burger.restaurant_id)
+    // }
 
     return (
         <div>
-            <ul>
             {props.burgers && props.burgers.map(burger => 
-                <li key={burger.id}>{burger.name} - {burger.description} - {burger.price}</li>
+                <li key={burger.id}>{burger.name} - {burger.description} - {burger.price}
+                    {/* <button onClick={() => handleDelete(burger)}>Delete</button> */}
+                </li>
+
             )}
-            </ul>
         </div>
     )
 }
 
-export default Burgers;
+// export default connect(null, {deleteBurger})(Burgers)
+export default Burgers
