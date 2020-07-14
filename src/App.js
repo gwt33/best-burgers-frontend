9907aadb2f8e415d7f1136fb8169d8e06552be8a
93geraldtee <93geraldtee@gmail.com>
2020-07-13 20:17:35 -0400

    add imports and h1 tag

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import NavBar from './components/NavBar.js'
 // import {fetchRestaurants} from './actions/fetchRestaurants'
 import RestaurantsContainer from './containers/RestaurantsContainer'
+import { Route, Switch } from 'react-router-dom';
+import RestaurantInput from './components/RestaurantInput';
+
 
 class App extends React.Component {
   
@@ -9,6 +13,11 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <h1><span role="img" aria-label="burger-icon">🍔</span> Best Burgers <span role="img" aria-label="burger-icon">🍔</span></h1>
+        <NavBar />
+        <Switch>
+          {/* <Route path="/restaurants/new" component={RestaurantInput}/> */}
+        </Switch>
         <RestaurantsContainer/>
       </div>
     );
