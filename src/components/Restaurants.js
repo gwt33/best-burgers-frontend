145340b1cf01f97f619f61c46a1c527de08e2f35
93geraldtee <93geraldtee@gmail.com>
2020-07-13 20:22:35 -0400

    remove delete import, button and function

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -1,16 +1,24 @@
 import React from 'react'
+import {connect} from 'react-redux'
 import {Route, Link} from 'react-router-dom';
 import Restaurant from './Restaurant';
+// import {deleteBurger} from '../actions/deleteBurger'
 
 // renders each restaurant
 const Restaurants = (props) => {
+
+    // const handleDelete = () => {
+    //     props.deleteBurger()
+    // }
+
     return (
         <div>
         <h1>Restaurants</h1>
             {props.restaurants.map(restaurant => 
                 <li key={restaurant.id}>
                     <Link to={`/restaurants/${restaurant.id}`}>{restaurant.name} - {restaurant.location}</Link>
-                </li> )}
+                {/* <button onClick={handleDelete}>Delete</button> */}
+                </li>  )} <br/>
         </div>
     )
 
