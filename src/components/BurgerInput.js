72cbb08a111faaff00189adaba2e2aad5d2329a9
93geraldtee <93geraldtee@gmail.com>
2020-07-13 20:21:17 -0400

    few syntax changes to debug submit burger

--- src/components/BurgerInput.js
+++ src/components/BurgerInput.js
@@ -1,37 +1,44 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import addBurger from '../actions/addBurger';
-// import BurgerInput from "../component/BurgerInput";
+import {createBurger} from '../actions/createBurger';
 
 class BurgerInput extends React.Component {
 
     state = {
         name: '',
         description: '',
-        price: ''
-    }
+        price: '',
 
+    }
+    
     handleChange = (event) => {
+        // debugger
         this.setState({
             [event.target.name]: event.target.value,
             [event.target.description]: event.target.value,
             [event.target.price]: event.target.value
         })
     }
-
-    handleSubmit = (event) => {
+    
+    handleSubmit = (event, _, props) => {
+        // debugger
         event.preventDefault()
-        // addBurger(this.state, this.props.id)
+        this.props.createBurger(this.state, this.props.restaurant.id)
+        this.setState({
+            name: '',
+            description: '',
+            price: ''
+        })
     }
 
     render() {
         return (
             <div>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <strong>Add a new Burger!</strong><br/>
-                    <input type='text' placeholder='Name' name="name" value={this.state.name} onChange={this.handleChange}/><br/>
-                    <input type='text' placeholder='Description' name="description" value={this.state.description} onChange={this.handleChange}/><br/>
-                    {/* <input type='integer' placeholder='Price' name="price" value={this.state.price} onChange={this.handleChange}/><br/> */}
+                    <input id={this.state.id} type='text' placeholder='Name' name="name" value={this.state.name} onChange={this.handleChange}/><br/>
+                    <input id={this.state.id} type='text' placeholder='Description' name="description" value={this.state.description} onChange={this.handleChange}/><br/>
+                    {/* <input id={this.state.id} type='text' placeholder='Price' name="price" value={this.state.price} onChange={this.handleChange}/><br/> */}
                     <input type="number" placeholder='Price' name="price" min="1" step="0.01" value={this.state.price} onChange={this.handleChange}/><br/>
 
                     <input type='submit'/>
@@ -41,4 +48,4 @@ class BurgerInput extends React.Component {
     }
 }
 
-export default connect(null)(BurgerInput);
+export default connect(null, {createBurger})(BurgerInput);
